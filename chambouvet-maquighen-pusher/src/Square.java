9379ae2b3928121650c1038e5b878d6809739f38
gaetan <gaetan.chambouvet-lauricella@iut-valence.fr>
2015-04-10 17:00:36 +0200

    Refactored Square class according to XItem enums

--- chambouvet-maquighen-pusher/src/Square.java
+++ chambouvet-maquighen-pusher/src/Square.java
@@ -1,14 +1,30 @@
 
 /**
- * 
+ * Contains a final fixed item, and could also contains a movable item which can change during the game
  * @author chamboug
  * 
  */
 public class Square 
 {
-	public Square()
+	/**
+	 *  Initial content
+	 */
+	private final FixedItem fixedContent;
+	
+	/**
+	 * Movable content
+	 */
+	private MovableItem movableContent;
+	
+	/**
+	 * Creation of a square
+	 * @param p_fixedItem the initial state of the square
+	 * @param p_movableItem 
+	 */
+	public Square(FixedItem p_fixedItem, MovableItem p_movableItem)
 	{
-		
+		this.fixedContent = p_fixedItem;
+		this.movableContent = p_movableItem;
 	}
 	
 }
