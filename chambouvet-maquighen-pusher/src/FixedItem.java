8e35270b49f0bf55d61e9ee49adddea0bbe8080d
gaetan <gaetan.chambouvet-lauricella@iut-valence.fr>
2015-04-10 16:44:01 +0200

    Turned FixedItem and MovableItem into enums
    - also removed GameItem (useless)

--- chambouvet-maquighen-pusher/src/FixedItem.java
+++ chambouvet-maquighen-pusher/src/FixedItem.java
@@ -1,4 +1,21 @@
 
-public class FixedItem extends GameItem {
+/**
+ * decor items, which can't move
+ * @author chamboug
+ *
+ */
+public enum FixedItem {
+	/**
+	 * Square which delimit the map, and where no movable item can pass
+	 */
+	WALL, 
+	/**
+	 * Empty square where every movable item can pass
+	 */
+	GROUND, 
+	/**
+	 * is a GROUND where there is a number associate to a unique box
+	 */
+	EXIT;
 
 }
--- chambouvet-maquighen-pusher/src/MovableItem.java
+++ chambouvet-maquighen-pusher/src/MovableItem.java
@@ -1,4 +1,23 @@
 
-public class MovableItem extends GameItem {
+/**
+ * Gameplay item which are movable
+ * @author chamboug
+ *
+ */
+public enum MovableItem  {
 
+	/**
+	 * no other movable item on the square
+	 */
+	NOTHING,
+	
+	/**
+	 * item which can be move by the PLAYER
+	 */
+	BOX,
+	
+	/**
+	 * character controled by the user
+	 */
+	PLAYER;
 }
