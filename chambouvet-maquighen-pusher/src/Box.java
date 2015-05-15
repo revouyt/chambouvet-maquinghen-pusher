145d7ceb427ec27cf9c025776c25deb337d2e425
maquingt <tanguy.maquinghen@iut-valence.fr>
2015-05-15 11:49:59 +0200

    make Box javadoc

--- chambouvet-maquighen-pusher/src/Box.java
+++ chambouvet-maquighen-pusher/src/Box.java
@@ -6,13 +6,24 @@
  */
 public class Box extends MovableItem
 {
+	/**
+	 * box number
+	 */
 	private int boxNumber;
 	
+	/**
+	 * 
+	 * @param p_boxNumber
+	 */
 	public Box (int p_boxNumber)
 	{
 		this.boxNumber = p_boxNumber;
 	}
 	
+	/**
+	 * get current box number
+	 * @return boxNumber
+	 */
 	public int getNumber()
 	{
 		return this.boxNumber;
