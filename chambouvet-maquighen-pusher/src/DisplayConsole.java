1a45c9aed8630443a2cda9cf1f632fbc04677a0a
gaetan <gaetan.chambouvet-lauricella@iut-valence.fr>
2015-05-20 08:21:42 +0200

    Complete communication's interface

--- chambouvet-maquighen-pusher/src/DisplayConsole.java
+++ chambouvet-maquighen-pusher/src/DisplayConsole.java
@@ -1,5 +1,5 @@
 
-public class DisplayConsole
+public class DisplayConsole implements PlayerDisplay
 {
 	public void displayDirectionChoice()
 	{
@@ -15,9 +15,20 @@ public class DisplayConsole
 		System.out.println("Victory !");
 	}
 	
+	public void displayInvalidDirectionMessage()
+	{
+		System.out.println("Invalid direction");
+	}
+	
+	public void displayHowToGiveUpMessage()
+	{
+		System.out.println("Type \"give up\" to stop your game");
+	}
+	
 	public void displayGiveUpMessage()
 	{
 		System.out.println("You give up, pusher's stronger than you!");
 	}
+
 
 }
