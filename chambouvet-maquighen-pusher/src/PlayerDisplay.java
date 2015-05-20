301f75e4098babf93f9681ebb22c39298cd136db
gaetan <gaetan.chambouvet-lauricella@iut-valence.fr>
2015-05-20 08:20:47 +0200

    Creation of the communication's interface

---
+++ chambouvet-maquighen-pusher/src/PlayerDisplay.java
@@ -0,0 +1,18 @@
+
+public interface PlayerDisplay
+{
+
+	
+	public void displayDirectionChoice();
+
+	public void displayMap(Map mapToDisplay);
+
+	public void displayVictoryMessage();
+	
+	public void displayInvalidDirectionMessage();
+
+	public void displayHowToGiveUpMessage();
+	
+	public void displayGiveUpMessage();
+
+}
---
+++ chambouvet-maquighen-pusher/src/PlayerEntry.java
@@ -0,0 +1,5 @@
+
+public interface PlayerEntry {
+
+	public Direction getDirectionToGo();
+}
