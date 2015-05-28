02d0c5606c698325c28f67941e9b713e154e6a1e
revouyt <tho.revouy@iut-valence.fr>
2015-05-28 12:30:51 +0200

    up

--- chambouvet-maquighen-pusher/src/GraphicalUserInterface.java
+++ chambouvet-maquighen-pusher/src/GraphicalUserInterface.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -16,8 +15,14 @@ import javax.swing.JPanel;
 public class GraphicalUserInterface extends JFrame
 {
 		
+	/**
+	 * Table where we put the game
+	 */
 	private JLabel[][] tableau;
 
+	/**
+	 * We recuperate the map of the game
+	 */
 	private Map map;
 	
 	
@@ -37,50 +42,42 @@ public class GraphicalUserInterface extends JFrame
 		
 		JPanel panel = new JPanel(); 
 		panel.setBounds(00, 0, 200, 500);
-		//JPanel panelboard[] = new JPanel[9];
-		GridLayout layout = new GridLayout(number_Of_Lines,number_Of_Columns,10,15);
-		//layout.setHgap(10);
-	    //layout.setVgap(10);
+		GridLayout layout = new GridLayout(number_Of_Columns,number_Of_Lines,10,15);
 	    panel.setLayout(layout);
-		//panel.add(panelboard[0]);
-		//panel.add(panelboard[1]);
-		//panelboard[0].setText("test");
-		//panelboard[1].setText("retest");
-		
-	    Icon Floor = new ImageIcon("G:\\Floor.png");
-	//    JLabel test = new JLabel();
-	 //   panel.add(test);
-	 //   test.setIcon(Floor); 
-	 //   this.add(test);
 		panel.setLayout(new FlowLayout());
-	//	JButton ButtonHelp = new JButton("Help");
-	//	panel.add(ButtonHelp);
-		//fenetre.getContentPane().add(new JButton("TEST"), BorderLayout.CENTER);
 		panel.setBackground(Color.WHITE);
-		//JButton ButtonExit = new JButton("Exit");
-		//panel.add(ButtonExit);
-		//JButton ButtonPlay = new JButton("Play");
-		//panel.add(ButtonPlay);
 		tableau = new JLabel[number_Of_Columns][number_Of_Lines];
-		for (int xmap = 0; xmap < number_Of_Columns; xmap++)
+		for (int ymap = 0; ymap < number_Of_Lines; ymap++)
 		{
-			for (int ymap = 0; ymap <  number_Of_Lines; ymap++)
+			for (int xmap = 0; xmap <  number_Of_Columns; xmap++)
 			{
 				JLabel cases=new JLabel();
 				tableau[xmap][ymap]=cases;
-				
-				Position currentposition = new Position(xmap,ymap);
-				if (map.getFixedContentAt(currentposition) == instanceof (Exit)) 
-				{
-					ImageIcon exit = new ImageIcon("bla")
-					cases.setIcon(exit);
-					panel.add(tableau[xmap][ymap]);
-				}
-				if getMovableContentAt
-					box
-				
+				Icon Floor = new ImageIcon("G:\\Floor.png");
 				cases.setIcon(Floor);
 				panel.add(tableau[xmap][ymap]);
+				
+			//	Position currentposition = new Position(xmap,ymap);
+			//	if (map.getFixedContentAt(currentposition)instanceof Exit) 
+			//	{
+			//		ImageIcon exit = new ImageIcon("G:\\Exit.png");
+			//		cases.setIcon(exit);
+			//		panel.add(tableau[xmap][ymap]);
+			 //   }
+			//	
+			//	if (map.getMovableContentAt(currentposition) instanceof Box)
+			//	{
+			//		ImageIcon box = new ImageIcon("G:\\Caisse.png");
+			//		cases.setIcon(box);
+			//	}
+			//	
+			//	if (map.getFixedContentAt(currentposition)instanceof Wall) 
+			//	{
+			//		ImageIcon wall = new ImageIcon("G:\\Wall.png");
+			//		cases.setIcon(wall);
+			//		panel.add(tableau[xmap][ymap]);
+			//   }
+
 			}
 		}
 		this.setContentPane(panel);
@@ -88,11 +85,10 @@ public class GraphicalUserInterface extends JFrame
 		
 		int positionPlayerx = map.getPlayerPosition().getX();
 		int positionPlayery = map.getPlayerPosition().getY();
-		ImageIcon tracto = new ImageIcon("G:\\Tractopellemaggle.png");
-		tableau[positionPlayerx][positionPlayery].setIcon(tracto);
-		
-		
+		ImageIcon tractopelle = new ImageIcon("G:\\Tractopellemaggle.png");
+		tableau[positionPlayerx][positionPlayery].setIcon(tractopelle);
+	
+		//JButton ButtonHelp = new JButton("Help");
+		//panel.add(ButtonHelp);
 		}
 }
-
-
